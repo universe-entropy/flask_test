96b13a67aa2ebfc142a1f2ed3701c4eb740196a8
Your Name <you@example.com>
2022-03-06 20:20:10 +0800

    Initial

---
+++ test_flask.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, render_template    # render template use jinja
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])  # decorator to link url to function
+def home():
+    return render_template('home.html')
+
+@app.route('/signin', methods=['GET'])
+def signin_form():
+    return render_template('form.html')
+
+@app.route('/signin', methods=['POST'])
+def signin():
+    username = request.form['username']
+    password = request.form['password']
+    if username=='admin' and password=='password':
+        return render_template('signin-ok.html', username=username)
+    else:
+        return render_template('form.html', message='Wrong username or password', username=username)
+
+if __name__ == '__main__':
+    app.run()    # flask default localhost:5000
